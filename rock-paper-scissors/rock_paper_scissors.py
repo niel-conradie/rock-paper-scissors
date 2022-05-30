bad98aa868a1c31b8f8aff2e317d5574402e2e3e
Niel Conradie <nielconradie93@gmail.com>
2022-05-30 11:52:14 +0200

    Update rock_paper_scissors.py
    Object-Oriented Programming Implementation.

---
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -0,0 +1,31 @@
+import random
+
+
+class RockPaperScissors:
+    """ A class to represent a Rock-Paper-Scissors game. """
+
+    @staticmethod
+    def user_input():
+        """ Requesting user input and validating choice. """
+        while True:
+            user_input = input("\nRock, Paper, Scissors! ").lower()
+            choices = ['rock', 'paper', 'scissors']
+            if user_input not in choices:
+                print(f"\n{user_input} is not an valid choice!")
+                continue
+            return user_input
+
+    @staticmethod
+    def computer_input():
+        """ Requesting computer input. """
+        choices = ['rock', 'paper', 'scissors']
+        computer_input = random.choice(choices)
+        return computer_input
+
+    @staticmethod
+    def round_win_condition(player_1, player_2):
+        """ Return True if player wins. """
+        if (player_1 == 'rock' and player_2 == 'scissors') or \
+           (player_1 == 'scissors' and player_2 == 'paper') or \
+           (player_1 == 'paper' and player_2 == 'rock'):
+            return True
