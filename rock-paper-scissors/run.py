7b504f33ce16bbaaddc49f5a9456512e5561a893
Niel Conradie <nielconradie93@gmail.com>
2022-05-30 11:55:16 +0200

    Create run.py
    Added run.py file to run game.

---
+++ rock-paper-scissors/run.py
@@ -0,0 +1,27 @@
+from rock_paper_scissors import RockPaperScissors
+
+
+def run():
+    """ Rock, Paper, Scissors. """
+    run = RockPaperScissors()
+
+    # Requesting user input.
+    user_input = run.user_input()
+    # Requesting computer input.
+    computer_input = run.computer_input()
+
+    # Player round win condition.
+    if run.round_win_condition(user_input, computer_input):
+        print("\nRound Won!")
+
+    # Computer round win condition.
+    if run.round_win_condition(computer_input, user_input):
+        print("\nRound Lost!")
+
+    # Round tie condition.
+    if user_input == computer_input:
+        print("\nTie!")
+
+
+if __name__ == '__main__':
+    run()
