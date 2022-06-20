3e6f867c34fa442843bfc91a5ef6ac200d49c243
Niel Conradie <nielconradie93@gmail.com>
2022-06-20 10:59:30 +0200

    Update run.py
    Added new methods to run.py file.

--- rock-paper-scissors/run.py
+++ rock-paper-scissors/run.py
@@ -8,21 +8,30 @@ def run():
     while True:
         # Requesting user input.
         user_input = run.user_input()
-
         # Requesting computer input.
         computer_input = run.computer_input()
 
         # Player round win condition.
         if run.round_win_condition(user_input, computer_input):
             print("\nRound Won!")
+            # Add point to player score.
+            run.add_player_score()
+            # Display scoreboard.
+            run.display_scoreboard(user_input, computer_input)
 
         # Computer round win condition.
         if run.round_win_condition(computer_input, user_input):
             print("\nRound Lost!")
+            # Add point to computer score.
+            run.add_computer_score()
+            # Display scoreboard.
+            run.display_scoreboard(user_input, computer_input)
 
         # Round tie condition.
         if user_input == computer_input:
             print("\nTie!")
+            # Display scoreboard.
+            run.display_scoreboard(user_input, computer_input)
 
         # Requesting user input.
         run.restart()
