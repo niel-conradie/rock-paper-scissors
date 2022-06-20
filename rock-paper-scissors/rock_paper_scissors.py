f36c025d7627d21b4a025fc7843d06e26ebe51fa
Niel Conradie <nielconradie93@gmail.com>
2022-06-20 10:58:17 +0200

    Update rock_paper_scissors.py
    Added point scoring system to class.

--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -4,6 +4,11 @@ import random
 class RockPaperScissors:
     """ A class to represent a Rock-Paper-Scissors game. """
 
+    def __init__(self):
+        """ Initialize class attributes. """
+        self.player_score = 0
+        self.computer_score = 0
+
     @staticmethod
     def user_input():
         """ Requesting user input and validating choice. """
@@ -29,6 +34,20 @@ class RockPaperScissors:
            (player_1 == 'scissors' and player_2 == 'paper') or \
            (player_1 == 'paper' and player_2 == 'rock'):
             return True
+
+    def add_player_score(self):
+        """ Add point to the player score. """
+        self.player_score += 1
+
+    def add_computer_score(self):
+        """ Add point to the computer score. """
+        self.computer_score += 1
+
+    def display_scoreboard(self, user_input, computer_input):
+        """ Display the choice and score. """
+        print(f"User: {user_input.title()}"
+              f"Computer: {computer_input.title()}"
+              f"{self.player_score} | {self.computer_score}")
 
     @staticmethod
     def restart():
