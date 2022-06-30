5f21100660fbc2c96ec556f58ad8ddd348dce61e
Niel Conradie <nielconradie93@gmail.com>
2022-06-30 12:19:56 +0200

    Update rock_paper_scissors.py
    Black code formatted file and import optimizations.

--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -1,20 +1,20 @@
-import random
+from random import choice
 
 
 class RockPaperScissors:
-    """ A class to represent a Rock-Paper-Scissors game. """
+    """A class to represent a Rock-Paper-Scissors game."""
 
     def __init__(self):
-        """ Initialize class attributes. """
+        """Initialize class attributes."""
         self.player_score = 0
         self.computer_score = 0
 
     @staticmethod
     def user_input():
-        """ Requesting user input and validating choice. """
+        """Requesting user input and validating choice."""
         while True:
             user_input = input("\nRock, Paper, Scissors! ").lower()
-            choices = ['rock', 'paper', 'scissors']
+            choices = ["rock", "paper", "scissors"]
             if user_input not in choices:
                 print(f"\n{user_input} is not an valid choice!")
                 continue
@@ -22,21 +22,23 @@ class RockPaperScissors:
 
     @staticmethod
     def computer_input():
-        """ Requesting computer input. """
-        choices = ['rock', 'paper', 'scissors']
-        computer_input = random.choice(choices)
+        """Requesting computer input."""
+        choices = ["rock", "paper", "scissors"]
+        computer_input = choice(choices)
         return computer_input
 
     @staticmethod
     def round_win_condition(player_1, player_2):
-        """ Return True if player wins. """
-        if (player_1 == 'rock' and player_2 == 'scissors') or \
-           (player_1 == 'scissors' and player_2 == 'paper') or \
-           (player_1 == 'paper' and player_2 == 'rock'):
+        """Return True if player wins."""
+        if (
+            (player_1 == "rock" and player_2 == "scissors")
+            or (player_1 == "scissors" and player_2 == "paper")
+            or (player_1 == "paper" and player_2 == "rock")
+        ):
             return True
 
     def game_win_condition(self):
-        """ Return True if player or computer score equals 3. """
+        """Return True if player or computer score equals 3."""
         if self.player_score == 3:
             print("\nWinner!")
             return True
@@ -45,37 +47,39 @@ class RockPaperScissors:
             return True
 
     def add_player_score(self):
-        """ Add point to the player score. """
+        """Add point to the player score."""
         self.player_score += 1
 
     def add_computer_score(self):
-        """ Add point to the computer score. """
+        """Add point to the computer score."""
         self.computer_score += 1
 
     def display_scoreboard(self, user_input, computer_input):
-        """ Display the choice and score. """
-        print(f"User: {user_input.title()}"
-              f"\nComputer: {computer_input.title()}"
-              f"\n{self.player_score} | {self.computer_score}")
+        """Display the choice and score."""
+        print(
+            f"User: {user_input.title()}"
+            f"\nComputer: {computer_input.title()}"
+            f"\n{self.player_score} | {self.computer_score}"
+        )
 
     def reset_score(self):
-        """ Reset player and computer scores to zero. """
+        """Reset player and computer scores to zero."""
         self.player_score = 0
         self.computer_score = 0
 
     @staticmethod
     def restart():
-        """ Requesting user input and validating choice. """
+        """Requesting user input and validating choice."""
         while True:
             user_input = input("\nRestart? Yes/No: ").lower()
-            choices = ['yes', 'no']
+            choices = ["yes", "no"]
             if user_input not in choices:
                 print("\nPlease type 'yes' or 'no'")
                 continue
 
             # User input conditions.
-            if user_input == 'yes':
+            if user_input == "yes":
                 return
-            if user_input == 'no':
+            if user_input == "no":
                 print("\nThank you for playing!")
                 quit()
