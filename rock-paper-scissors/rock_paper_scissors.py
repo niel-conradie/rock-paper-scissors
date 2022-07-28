b92093ba468fe7092d3ba020c3e4563e21e101ba
Niel Conradie <nielconradie93@gmail.com>
2022-07-28 13:03:46 +0200

    Update rock_paper_scissors.py
    Removed unnecessary comments.

--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -82,7 +82,7 @@ class RockPaperScissors:
         self.computer_score = 0
 
     def start_game(self):
-        """Starting the game."""
+        """Starting the rock-paper-scissors game."""
         while True:
             print("\nFirst to '3' wins!")
 
@@ -95,28 +95,22 @@ class RockPaperScissors:
                 # Player round win condition.
                 if self.round_win_condition(user_input, computer_input):
                     print("\nRound Won!")
-                    # Add point to player score.
                     self.add_player_score()
-                    # Display scoreboard.
                     self.display_scoreboard(user_input, computer_input)
 
                 # Computer round win condition.
                 if self.round_win_condition(computer_input, user_input):
                     print("\nRound Lost!")
-                    # Add point to computer score.
                     self.add_computer_score()
-                    # Display scoreboard.
                     self.display_scoreboard(user_input, computer_input)
 
                 # Round tie condition.
                 if user_input == computer_input:
                     print("\nTie!")
-                    # Display scoreboard.
                     self.display_scoreboard(user_input, computer_input)
 
                 # Game win condition.
                 if self.game_win_condition() == True:
-                    # Reset the scores to zero.
                     self.reset_score()
                     break
                 else:
@@ -132,7 +126,7 @@ class RockPaperScissors:
         """Requesting user input and validating choice."""
         while True:
             print("\nPlay Again?")
-            print("Yes: Type '1'")
+            print("\nYes: Type '1'")
             print("No: Type '2'")
 
             try:
